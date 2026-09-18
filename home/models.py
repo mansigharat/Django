03ce7bf18a09b66b06516480e9ff118f067bb421
Mansi Gharat <mansigharat1909@gmail.com>
2026-09-18 15:41:51 +0530

    models and migrations

--- home/models.py
+++ home/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
 class Student(models.Model):
+   # id = models.AutoField()
     name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField(null=True, blank=True)
+    image = models.ImageField()
+    file = models.FileField()
+
+class Product(models.Model):
+    pass
+
