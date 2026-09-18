6eb37e4400f2af719846f96b5b82447540941786
Mansi Gharat <mansigharat1909@gmail.com>
2026-09-18 16:19:00 +0530

    shell command

---
+++ home/utils.py
@@ -0,0 +1,14 @@
+from home.models import Student
+import time
+
+def run_this_function():
+    print("Test 1 pass ✅️")
+    time.sleep(2)
+    print("Test 2 pass ✅️")
+    time.sleep(2)
+    print("Test 3 pass ✅️")
+    time.sleep(2)
+    print("Test 4 pass ✅️")
+    time.sleep(2)
+    print("Test 5 pass ✅️")
+    time.sleep(2)
