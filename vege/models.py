15272b2a954edc4155c589e3ac51f80b691e4a4c
Mansi Gharat <mansigharat1909@gmail.com>
2026-09-25 14:26:01 +0530

    Add 'receipe_view_count' field to Recipe model and create migration

--- vege/models.py
+++ vege/models.py
@@ -6,4 +6,5 @@ class Recipe(models.Model):
     receipe_name = models.CharField(max_length = 100)
     receipe_description = models.TextField()
     receipe_image = models.ImageField(upload_to = "receipe/")
+    receipe_view_count = models.IntegerField(default=1)
     
